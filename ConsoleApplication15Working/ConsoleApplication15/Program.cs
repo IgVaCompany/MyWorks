89e280e59ec26352f30c82b207cb5fbe6b9630cc
VasudevGhosh <34122704+VasudevGhosh@users.noreply.github.com>
2017-12-20 14:49:10 +0300

    11

--- ConsoleApplication15Working/ConsoleApplication15/Program.cs
+++ ConsoleApplication15Working/ConsoleApplication15/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,7 @@ namespace ConsoleApplication15
         string [] Commands = new string[8] { "Close","ShowC" ,"DLFF", "PDFF" ,"CalcAtom", "CalcMolec","td","calc" };
         private bool atomFlag = false;
         private bool molecFlag = false;
+        public static bool dlff = false;
 
         Particle SomeBody = new Particle();
         Atom He = new Atom();
@@ -28,8 +30,7 @@ namespace ConsoleApplication15
             
             string Command = "Commad is wrong write again";          
             string InPutCommand =  Console.ReadLine();
-            
-         
+                     
 
             for (int i = 0; i < Commands.Length; i++)
             {
@@ -42,19 +43,30 @@ namespace ConsoleApplication15
             {
                 case "CalcAtom":
                     atomFlag = true;
-                    molecFlag = false;  
-                    He.DL();                 
+                    molecFlag = false;
+                    do
+                    {
+                        He.DL();
+                    } while (!dlff);
+                                      
                     break;
                 case "CalcMolec":
                     atomFlag = false;
-                    molecFlag = true;                  
-                    CC.DL();
+                    molecFlag = true;
+                    do
+                    {
+                        CC.DL();                       
+                    } while (!dlff);
+                    CC.CalcEi_n();
+                    CC.CalcEj_i();
+                    CC.CalcEkin();
+                    CC.TotalCalc();
                     break;
                 case "Close":
                    Environment.Exit(0);
                     break;
                 case "DLFF":
-                    SomeBody.DownLoadDataFromFile();                 
+                                  
                     break;
                 case "PDFF":
                     if (atomFlag && !molecFlag)
@@ -88,47 +100,62 @@ namespace ConsoleApplication15
     {
         public double Totalenergy;
 
-        public string[] DataParamsNames = new string[4] { "Massa", "PotEnergy", "KinEnergy", "Stepeny" };
-        public static  double[] DataProgram = new double[4];
-
         public string[] DataFile;
-        private string NameFile;
+        private string NameFile; 
+        
+        public const double h = 6.626070040e-34;
+        public const double R = 1.38064852e-23;
+        public const double vC = 299792458;
+        public const int temper = 300;
 
-        public static bool DLFF = false;
-
-        public void DownLoadDataFromFile()
-        {
-            Console.WriteLine("Input the file name" + "\n");
-            string NameFile = Console.ReadLine();
-            string[] DataFile = File.ReadAllLines(NameFile);
-
-            DLFF = true;
-            for (int i = 0; i < (DataFile.Length - 1); i++)
-            {
-                for (int j = 0; j < DataParamsNames.Length; j++)
-                {
-                    if (DataFile[i] == DataParamsNames[j])
-                        DataProgram[j] = Convert.ToDouble(DataFile[i + 1]);
-                }
-            }
-            Console.WriteLine("DownLload is OK");
-        }
+        public double tEkin;
+        public double tE;
+        public static int numOfLevels;
 
         public virtual void DL()
         {
-            Console.WriteLine("Input the file name" + "\n");                   
-            NameFile = Console.ReadLine();
-            DataFile = File.ReadAllLines(NameFile);
-          
+            again: 
+            try
+            {
+                Console.WriteLine("Input the file name" + "\n");
+                NameFile = Console.ReadLine();
+                DataFile = File.ReadAllLines(NameFile);
+            }
+            catch (Exception)
+            {
+                goto again;
+                throw;
+            }                   
         }
 
-        public virtual void CalcEnergy()
+        public virtual void CalcEi_n()
         {
+            if (numOfLevels == 0) ;
+            {
+                Console.WriteLine("Put level numbers");
+                numOfLevels = Convert.ToInt32(Console.ReadLine());
+            }
 
         }
-        public virtual void calc()
+        public virtual void CalcEj_i()
         {
+            if (numOfLevels == 0)
+            {
+                Console.WriteLine("Put level numbers");
+                numOfLevels = Convert.ToInt32(Console.ReadLine());
+            }
+            
+        }
 
+        public virtual void CalcEkin()
+        {
+            tEkin = (3/2)*R*temper;           
+        }
+
+        public virtual void TotalCalc()
+        {
+            Console.WriteLine("Total Energy");
+            Console.WriteLine(tE);
         }
 
         public void PrintRelut(string result)
@@ -139,16 +166,16 @@ namespace ConsoleApplication15
 
     public class Atom : Particle
     {
-        List<double> config = new List<double>();
-        List<double> jj = new List<double>();
+        private string[] needDataForAtom = new string[3] { "Configuration", "J", "Level"};
+        List<string> config = new List<string>();
+        List<int> jj = new List<int>();
         List<double> level = new List<double>();
 
         public override void DL()
         {
             base.DL();
             String valueName = DataFile[0];
-            String[] valueNames = valueName.Split('\t', ' ', '-', '|');
-            Console.WriteLine(valueNames.Length);
+            String[] valueNames = valueName.Split(new char[] {'\t', ' ', '-', '|', '[', ']'},StringSplitOptions.RemoveEmptyEntries);
 
             foreach (var vals in valueNames)
             {
@@ -157,16 +184,16 @@ namespace ConsoleApplication15
             for (int i = 1; i < DataFile.Length; i++)
             {
                 String value = DataFile[i];
-                String[] vaslues = value.Split('\t', ' ', '-', '|','[',']');
+                String[] vaslues = value.Split(new char[] { '\t', ' ', '-', '|', '[', ']' }, StringSplitOptions.RemoveEmptyEntries);
                 for (int j = 0; j < vaslues.Length; j++)
                 {
                     switch (valueNames[j])
                     {
                         case "Configuration":
-                            config.Add(Convert.ToDouble(vaslues[j]));
+                            config.Add(vaslues[j]);
                             break;
                         case "J":
-                            jj.Add(Convert.ToDouble(vaslues[j]));
+                            jj.Add(Convert.ToInt16(vaslues[j]));
                             break;
                         case "Level":
                             level.Add(Convert.ToDouble(vaslues[j]));
@@ -196,16 +223,16 @@ namespace ConsoleApplication15
             }
         }
 
-        public override void CalcEnergy()
+        public override void CalcEi_n()
         {
-            base.CalcEnergy();
-           // Totalenergy = KineticEnergy + PotetionalEnergy;
-            Totalenergy = DataProgram[0] + DataProgram[1] + DataProgram[2] + DataProgram[3];
+            base.CalcEi_n();
         }
     }
 
     public class Molecule : Particle
     {
+        private string[] needDataForMolec = new string[8] { "te", "gi", "omegaE", "omegaExE", "bE", "alfaE", "dE", "bettaE" };
+        List<string> dlData =new List<string>();
         List<double> te = new List<double>();
         List<double> gi = new List<double>();
         List<double> omegaE = new List<double>();
@@ -215,48 +242,78 @@ namespace ConsoleApplication15
         List<double> dE = new List<double>();
         List<double> bettaE = new List<double>();
 
+        public double tEi_n;
+        public double tEj_i;
+        List<double> Ei = new List<double>();
+        List<double> Ej_i = new List<double>();
+       
 
         public override void DL()
         {           
             base.DL();
             String valueName = DataFile[0];
-            String[] valueNames = valueName.Split('\t', ' ');
-            Console.WriteLine(valueNames.Length);
-            for (int i = 1; i < DataFile.Length; i++)
-            {
-                String value = DataFile[i];
-                String[] vaslues = value.Split('\t');                
-                for (int j = 0; j < vaslues.Length; j++)
+            String[] valueNames = valueName.Split('\t', ' ');           
+                for (int i = 0; i < valueNames.Length; i++)
                 {
-                    switch (valueNames[j])
+                    for (int j = 0; j < needDataForMolec.Length; j++)
                     {
-                        case "te":
-                            te.Add(Convert.ToDouble(vaslues[j]));
-                            break;
-                        case "gi":
-                            gi.Add(Convert.ToDouble(vaslues[j]));
-                            break;
-                        case "omegaE":
-                            omegaE.Add(Convert.ToDouble(vaslues[j]));
-                            break;
-                        case "omegaExE":
-                            omegaExE.Add(Convert.ToDouble(vaslues[j]));
-                            break;
-                        case "bE":
-                            bE.Add(Convert.ToDouble(vaslues[j]));
-                            break;
-                        case "alfaE":
-                            alfaE.Add((Convert.ToDouble(vaslues[j]))*Math.Pow(10,2));
-                            break;
-                        case "dE":
-                            dE.Add((Convert.ToDouble(vaslues[j]))*Math.Pow(10,6));
-                            break;
-                        case "bettaE":
-                            bettaE.Add((Convert.ToDouble(vaslues[j]))*Math.Pow(10,6));
-                            break;
+                        if (valueNames[i] == needDataForMolec[j])
+                        {
+                            dlData.Add(needDataForMolec[j]);
+                        }
                     }
-                }              
-            }                   
+                }
+                if (dlData.ToArray().Length == needDataForMolec.Length)
+                {
+                    for (int i = 1; i < DataFile.Length; i++)
+                    {
+                        String value = DataFile[i];
+                        String[] vaslues = value.Split('\t');
+
+                        for (int j = 0; j < vaslues.Length; j++)
+                        {
+                            switch (valueNames[j])
+                            {
+                                case "te":
+                                    te.Add(Convert.ToDouble(vaslues[j]));
+                                    break;
+                                case "gi":
+                                    gi.Add(Convert.ToDouble(vaslues[j]));
+                                    break;
+                                case "omegaE":
+                                    omegaE.Add(Convert.ToDouble(vaslues[j]));
+                                    break;
+                                case "omegaExE":
+                                    omegaExE.Add(Convert.ToDouble(vaslues[j]));
+                                    break;
+                                case "bE":
+                                    bE.Add(Convert.ToDouble(vaslues[j]));
+                                    break;
+                                case "alfaE":
+                                    alfaE.Add((Convert.ToDouble(vaslues[j]))*Math.Pow(10, 2));
+                                    break;
+                                case "dE":
+                                    dE.Add((Convert.ToDouble(vaslues[j]))*Math.Pow(10, 6));
+                                    break;
+                                case "bettaE":
+                                    bettaE.Add((Convert.ToDouble(vaslues[j]))*Math.Pow(10, 6));
+                                    break;
+                            }
+                        }
+                    }
+                  CommandsWork.dlff = true;
+                  Console.WriteLine("DownLoad OK");
+            }
+                else
+                {
+                    Console.WriteLine("Data is not correct" + "\nDownLoad please");
+                    foreach (var s in needDataForMolec)
+                    {
+                        Console.Write(s + " ");
+                    }
+                    Console.WriteLine("Input the file name" + "\n");
+
+                }            
         }
 
         public void PrintDataDL()
@@ -303,17 +360,80 @@ namespace ConsoleApplication15
             }
         }
 
-        public override void CalcEnergy()
+        public override void CalcEi_n()
         {
-            base.CalcEnergy();
-            Totalenergy = DataProgram[0] + DataProgram[1] + DataProgram[2] + DataProgram[3];
+            base.CalcEi_n();
+            for (int i = 0; i < numOfLevels; i++)
+            {
+                try
+                {
+                    Ei.Add(Particle.h * Particle.vC * (omegaE.ToArray()[i] * (i + 0.5f) - omegaExE.ToArray()[i] * Math.Pow((i + 0.5f), 2)));
+                    tEi_n += Ei.ToArray()[i];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Load more Data of Lelels & Calc Again");
+                    //throw;
+                    break;
+                }               
+            }
+            Console.WriteLine("Total Ei: "+ tEi_n);
+            int f = 1;
+            foreach (var s in Ei)
+            {
+                Console.WriteLine(f+" "+s);
+                f++;
+            }            
+           
+        }
+        public override void CalcEj_i()
+        {
+            base.CalcEj_i();
+            for (int i = 0; i < numOfLevels; i++)
+            {
+                try
+                {
+                    double Bcn = bE.ToArray()[i] - alfaE.ToArray()[i] * (i + 0.5f);
+                    double Dcn = dE.ToArray()[i] - bettaE.ToArray()[i] * (i + 0.5f);
+                    Ej_i.Add(Particle.h * Particle.R * (Bcn * i * (i + 1) - Dcn * Math.Pow(i, 2) * Math.Pow((i + 1), 2)));
+                    tEj_i += Ej_i.ToArray()[i];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Load more Data of Lelels & Calc Again");
+                    //throw;
+                    break;
+                }
+               
+            }
+            Console.WriteLine("Total Eji: " + tEj_i);
+            int f = 1;
+            foreach (var s in Ej_i)
+            {
+                Console.WriteLine(f + " " + s);
+                f++;
+            }
+        }
+
+        public override void CalcEkin()
+        {
+            base.CalcEkin();
+            Console.WriteLine("Kinetic Energy Molec: " + tEkin);
+        }
+
+        public override void TotalCalc()
+        {           
+            tE = tEi_n + tEj_i + tEkin;
+            base.TotalCalc();
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-                     
+            int with = 150;
+            int hegith = 40;
+            Console.SetWindowSize(with, hegith);     
             string command;
             CommandsWork Application = new CommandsWork();
             do
